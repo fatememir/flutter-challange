398d86829014a348458cc395779917cc66fc4bc8
fatememir <fa.mirr74@gmail.com>
2024-04-24 00:33:57 +0330

    fix bug(add getMapListBloc to blocProvider)

--- lib/core/bloc/app_bloc_provider.dart
+++ lib/core/bloc/app_bloc_provider.dart
@@ -1,8 +1,8 @@
-
-
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../features/login/presentation/bloc/login_bloc.dart';
 import '../../features/login/presentation/screens/login_screen.dart';
+import '../../features/stadium_seats/presentation/bloc/get_maps_list/get_maps_list_bloc.dart';
+import '../../features/stadium_seats/presentation/screens/stadium_seats_screen.dart';
 import '../injection/injection.dart';
 
 class AppBlocProviders {
@@ -11,6 +11,10 @@ class AppBlocProviders {
       BlocProvider<LoginBloc>(
         create: (_) => getIt(),
         child: const LoginScreen(),
+      ),
+      BlocProvider<GetMapsListBloc>(
+        create: (_) => getIt(),
+        child: const StadiumSeatsScreen(),
       ),
     ];
   }
